4a9123c987f28f6b00ccdd458f1b05ae9c8fff64
thomasyang18 <tomyangtt18@gmail.com>
2019-02-19 18:47:33 -0500

    Player Movement
    Player basic physics and sprites finished. Will now work on attack, maps, and finally balance. This is supposed to be a silly game, but there has to be some balance in the game.

---
+++ Chicken Fight 2P/Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator anim;
+    public GameObject p;
+    private PlayerStats player;
+    private SpriteRenderer render;
+
+    void Start()
+    {
+        player = p.GetComponent<PlayerStats>();
+        anim = GetComponent<Animator>();
+        render = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.getX() < 0)
+        {
+            render.flipX = false;
+        }
+        else if (player.getX() > 0){
+            render.flipX = true;
+        }
+        anim.SetFloat("Speed", player.getAbsSpeed());
+        anim.SetBool("inAir", player.getInAir());
+    }
+}
---
+++ Chicken Fight 2P/Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed= 1f;
+    public float jumpForce = 1f;
+    private float x;
+    private Rigidbody2D rb;
+    private bool airborne;
+    private Transform trans;
+    public Collider2D groundCheck;
+    
+    void Start()
+    {
+        x = 0f;
+        rb = GetComponent<Rigidbody2D>();
+        trans = GetComponent<Transform>();
+        airborne = false;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        x = Input.GetAxisRaw("HorizontalP1");
+        rb.AddForce(Vector2.right * x * speed);
+       
+        float y = Input.GetAxisRaw("VerticalP1");
+        if (y == 1f && airborne == false) {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            airborne = true;
+        }
+    }
+
+    public bool getInAir() {
+        return airborne;
+    }
+
+    public float getAbsSpeed() {
+        return Mathf.Abs(x);
+    }
+    public float getX() {
+        return x;
+    }
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Floor")
+        {
+            airborne = false;
+            Debug.Log("We collide with the floor ");
+        }
+        Debug.Log("We Collided");
+    }
+}
