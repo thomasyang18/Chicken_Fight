597c300ef2759efe0d8fa3040a058cd4e8327b20
thomasyang18 <tomyangtt18@gmail.com>
2019-02-20 14:58:45 -0500

    Player Prefab, Map
    The player prefab is mostly done, and the map is set up (all we need to do is use gimp to set up tiles via gimp)

---
+++ Chicken Fight 2P/Assets/Scripts/EggProjectileBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EggProjectileBehavior : MonoBehaviour
+{
+    private int player;
+    void Update() {
+    }
+
+    
+
+    void OnTriggerEnter2D(Collider2D other) {
+        //Debug.Log("egg hit something");
+
+        if (other.gameObject.tag == "Player") {
+            Destroy(other.gameObject);
+        }
+        if (other.gameObject.tag == "Floor")
+        {
+            Destroy(gameObject);
+            //Debug.Log("egg hit floor");
+        }
+    }
+
+    
+}
--- Chicken Fight 2P/Assets/Scripts/PlayerStats.cs
+++ Chicken Fight 2P/Assets/Scripts/PlayerStats.cs
@@ -12,26 +12,47 @@ public class PlayerStats : MonoBehaviour
     private bool airborne;
     private Transform trans;
     public Collider2D groundCheck;
+    private float reloadTime;
+    public float reloadDelay = 1f;
+    public Rigidbody2D egg;
+    public Transform eggSpawn;
+
+    public int player;
+    private string[] axes = {"HorizontalP" , "VerticalP" , "FireP" }; // 0 is horizontal, 1 is vertical, 2 is fire.
+
+    public AudioSource eggShot;
     
     void Start()
     {
+        for (int i = 0; i < axes.Length; i++) {
+            axes[i] += player;
+        }
         x = 0f;
         rb = GetComponent<Rigidbody2D>();
         trans = GetComponent<Transform>();
         airborne = false;
+        reloadTime = reloadDelay;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        x = Input.GetAxisRaw("HorizontalP1");
+        x = Input.GetAxisRaw(axes[0]);
         rb.AddForce(Vector2.right * x * speed);
        
-        float y = Input.GetAxisRaw("VerticalP1");
+        float y = Input.GetAxisRaw(axes[1]);
         if (y == 1f && airborne == false) {
             rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
             airborne = true;
         }
+        reloadTime += Time.deltaTime;
+        if (reloadTime > reloadDelay && Input.GetAxisRaw(axes[2]) == 1) {
+            reloadTime = 0;
+            Instantiate(egg, eggSpawn.position, transform.rotation);
+            eggShot.Play();
+        }
+        // Debug.Log("reloadTime: " + reloadTime);
+
     }
 
     public bool getInAir() {
@@ -44,13 +65,15 @@ public class PlayerStats : MonoBehaviour
     public float getX() {
         return x;
     }
+    public int playerNumber() {
+        return player;
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Floor")
         {
             airborne = false;
-            Debug.Log("We collide with the floor ");
         }
-        Debug.Log("We Collided");
+        //Debug.Log("We Collided");
     }
 }
