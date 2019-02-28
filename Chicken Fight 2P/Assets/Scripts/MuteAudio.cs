c8cfcd5601a72c84f195b1dbad750fb40d5f4c66
thomasyang18 <tomyangtt18@gmail.com>
2019-02-27 21:09:52 -0500

    Final version
    Background music. My very first legit game

---
+++ Chicken Fight 2P/Assets/Scripts/MuteAudio.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MuteAudio : MonoBehaviour
+{
+    public Text toggleText;
+    private AudioSource a;
+    // Start is called before the first frame update
+    void Start()
+    {
+        a = GetComponent<AudioSource>();
+        UpdateText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Mute")) {
+            a.enabled = !a.enabled;
+        }
+        UpdateText();
+    }
+
+
+    void UpdateText() {
+        if (a.enabled)
+        {
+            toggleText.text = "MUSIC: ON";
+        }
+        else {
+            toggleText.text = "MUSIC: OFF";
+        }
+    }
+}
